670a766665c099a85dd46e5cfbccffa711797ab5
Myoungeun <Myoungeun@DESKTOP-12591M3>
2022-01-18 21:35:59 +0900

    array test

---
+++ Array/src/ArPractice.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArPractice {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList(); //ArrayList
+	      
+	      al.add(14); //add
+	      al.add(-1);
+	      al.add(22);
+	      al.add(32);
+	      for (int i=0; i< al.size();i++) { //.size()
+	         System.out.println(al.get(i)); //get(i)
+	      }
+		
+
+	}
+
+}
