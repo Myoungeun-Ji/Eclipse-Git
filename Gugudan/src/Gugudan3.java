61497216611707385fabab48829d8f449cd8ad42
Myoungeun <Myoungeun@DESKTOP-12591M3>
2022-01-18 21:59:29 +0900

    gugudan test

---
+++ Gugudan/src/Gugudan3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Gugudan3 {
+
+	public static void main(String[] args) {
+		int j=1, i=0;
+		Scanner s=new Scanner(System.in);
+		Scanner si=new Scanner(System.in);
+//		i=s.nextInt(); //단수 입력받기
+		i=Integer.parseInt(s.nextLine());
+//		int n=si.nextInt();	
+		for(j=1; j<=9; j++) {
+				System.out.println(i+" x "+j+" = "+3*j);
+			}
+	}}
+		
+
+
